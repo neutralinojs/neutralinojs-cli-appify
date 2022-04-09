4fb7530886208376fcfbbbd5f50dc601d052986f
Ayman161803 <mohammedaymannawaz@gmail.com>
2022-04-09 21:20:42 +0530

    Improved Extraction of title

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ module.exports = {
         command
             .option('-t,--title [title]')
             .action(async (url, command) => {
+                let hostname = new URL(url).hostame
+                hostname = hostame.split(".")
+                let titleExtractedFromURL
+                if(hostame.length>2){
+                    titleExtractedFromURL = hostname[1]
+                }
+                else{
+                    titleExtractedFromURL = hostname[0]
+                }
                 // Make app 
-                let title = (command.title|| new URL(url).hostame || 'Appify');
+                let title = (command.title|| titleExtractedFromURL || 'Appify');
                 let appId = title.toLowerCase().replace(/ /g, '.');
                 let binaryName = title.toLowerCase().replace(/ /g, '-');
                 await modules.creator.createApp(binaryName);
