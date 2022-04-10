8d6003a2a8492a43091a6f4cebacdf4ac31bc358
Nethmi Rodrigo <neth23rodrigo@gmail.com>
2022-04-10 10:23:32 +0530

    Fix close process on exit

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 module.exports = {
-    command: 'appify <url>',
-    register: (command, modules) => {
-        command
-            .option('-t,--title [title]')
-            .action(async (url, command) => {
-                // Make app 
-                let title = (command.title || 'Appify');
-                let appId = title.toLowerCase().replace(/ /g, '.');
-                let binaryName = title.toLowerCase().replace(/ /g, '-');
-                await modules.creator.createApp(binaryName);
-                modules.config.update('url', url);
-                modules.config.update('applicationId', appId);
-                modules.config.update('modes.window.title', title);
-                modules.config.update('modes.window.enableInspector', false);
-                // Bundle it
-                await modules.bundler.bundleApp(true);
-                console.log(`Please check the .${binaryName}/dist folder and find your desktop app.`);
-            })
-    }
+  command: "appify <url>",
+  register: (command, modules) => {
+    command.option("-t,--title [title]").action(async (url, command) => {
+      // Make app
+      let title = command.title || "Appify";
+      let appId = title.toLowerCase().replace(/ /g, ".");
+      let binaryName = title.toLowerCase().replace(/ /g, "-");
+      await modules.creator.createApp(binaryName);
+      modules.config.update("url", url);
+      modules.config.update("applicationId", appId);
+      modules.config.update("modes.window.title", title);
+      modules.config.update("modes.window.enableInspector", false);
+      modules.config.update("modes.window.exitProcessOnClose", true);
+      // Bundle it
+      await modules.bundler.bundleApp(true);
+      console.log(
+        `Please check the .${binaryName}/dist folder and find your desktop app.`
+      );
+    });
+  },
 };
