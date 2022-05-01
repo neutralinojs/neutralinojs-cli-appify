f23c31a2211c7d14a09da1c35eae01fd55288d10
Ayman <76606666+Ayman161803@users.noreply.github.com>
2022-05-01 11:58:43 +0530

    Extract title from parsed webpage fetched through URL (#10)
    * Removed $ from commands
    
    * Set default title as hostname
    
    * Update readme.md
    
    * Update readme.md
    
    * Update readme.md
    
    * Improved Extraction of title
    
    * parsed webpage and used regex to get title from tiitle tag
    
    * removed unnecessary async
    
    * improved indentation
    
    * Update index.js
    
    Co-authored-by: Shalitha Suranga <shalithasuranga@gmail.com>

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+const https = require('https');
+
 module.exports = {
     command: 'appify <url>',
     register: (command, modules) => {
         command
             .option('-t,--title [title]')
             .action(async (url, command) => {
+         
                 // Make app 
-                let title = (command.title || 'Appify');
+                let title = command.title || await getTitle(url) || 'Appify';
                 let appId = title.toLowerCase().replace(/ /g, '.');
                 let binaryName = title.toLowerCase().replace(/ /g, '-');
                 await modules.creator.createApp(binaryName);
@@ -13,9 +16,32 @@ module.exports = {
                 modules.config.update('applicationId', appId);
                 modules.config.update('modes.window.title', title);
                 modules.config.update('modes.window.enableInspector', false);
+         
                 // Bundle it
                 await modules.bundler.bundleApp(true);
                 console.log(`Please check the ${binaryName}/dist folder and find your desktop app.`);
             })
     }
 };
+
+
+function getTitle(url) {
+ const TITLE_REGEX = /<title>(.*)<\/title>/im;
+ 
+ return new Promise((resolve, reject)=>{  
+    https.get(url, (res) => {
+      let body = '', title = '';
+      res.on('data',  (chunk) => {
+        body += chunk;
+        if(!title && TITLE_REGEX.test(body)) {
+          title = body.match(TITLE_REGEX)[1];
+        }
+      });
+      res.on('end', () => {
+          resolve(title);
+      });
+    }).on('error', (err) => {
+        reject('');
+    });
+  })
+}
